77322994f31bb8221bade638afdd55343d65c497
Сергей Мосин <mosinsv@yandex.ru>
2019-11-06 13:39:21 +0400

    JS Animation & push for last examine

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var form_wrapper = document.querySelector(".form-wrapper");
 var form_show = document.querySelector(".form-show");
+var hotel_search = document.querySelector(".hotel-search");
 var arrival_date = form_wrapper.querySelector("[id=arrival-date]");
 var departure_date = form_wrapper.querySelector("[id=departure-date]");
 var old = form_wrapper.querySelector("[id=old]");
@@ -7,15 +8,14 @@ var old = form_wrapper.querySelector("[id=old]");
 form_show.addEventListener("click", function (evt) {
     evt.preventDefault();
     form_wrapper.classList.toggle("form-wrapper-hide");
-    form_wrapper.classList.toggle("form-wrapper-show");
     form_wrapper.classList.remove("form-wrapper-error");
 });
 
-form_wrapper.addEventListener("submit", function (evt) {
+hotel_search.addEventListener("submit", function (evt) {
     if (!arrival_date.value || !departure_date.value || !old.value) {
     evt.preventDefault();
-    document.classList.remove("form-wrapper-error");
+    form_wrapper.classList.remove("form-wrapper-error");
     form_wrapper.offsetWidth = form_wrapper.offsetWidth;
-    document.classList.add = ("form-wrapper-error");
+    form_wrapper.classList.add("form-wrapper-error");
   }
 });
